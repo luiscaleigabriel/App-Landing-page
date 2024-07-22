b28b742444c7d61d40d34af84f536fbb75f6523a
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-07-22 18:03:54 +0100

    ...

--- src/components/Feacture.jsx
+++ src/components/Feacture.jsx
@@ -1,12 +1,15 @@
 import FeactureBox from "./FeactureBox";
+import image1 from '../../public/images/feature_1.png'
+import image2 from '../../public/images/feature_2.png'
+import image3 from '../../public/images/feature_3.png'
 
 export default function Feacture() {
   return (
     <div className="features">
       <div className="a-container">
-        <FeactureBox title='Curso de Desenvolvimento' image='/images/feature_1.png' />
-        <FeactureBox title='Serviços' image='/images/feature_2.png' />
-        <FeactureBox title='Usabilidade da Interface' image='/images/feature_3.png' />
+        <FeactureBox title='Curso de Desenvolvimento' image={image1} />
+        <FeactureBox title='Serviços' image={image2} />
+        <FeactureBox title='Usabilidade da Interface' image={image3} />
       </div>
     </div>
   )
